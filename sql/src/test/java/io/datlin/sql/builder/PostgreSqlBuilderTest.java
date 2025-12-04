4ed9d9eb37bab8bd04f8c1e62fe52f9b2daf37b5
Paweł Bobryk <bobryk.pawel@gmail.com>
2025-12-04 22:05:51 +0100

    sync

---
+++ sql/src/test/java/io/datlin/sql/builder/PostgreSqlBuilderTest.java
@@ -0,0 +1,13 @@
+package io.datlin.sql.builder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostgreSqlBuilderTest {
+
+    @Test
+    void build() {
+        System.out.printf("PostgreSqlBuilderTest.build()\n");
+    }
+}
