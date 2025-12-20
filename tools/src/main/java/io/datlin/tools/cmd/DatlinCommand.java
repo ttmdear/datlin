ef116cd156ec9e55ee4cb84c0191a44c251b20fa
Paweł Bobryk <bobryk.pawel@gmail.com>
2025-12-20 14:18:38 +0100

    sync

--- tools/src/main/java/io/datlin/tools/cmd/DatlinCommand.java
+++ tools/src/main/java/io/datlin/tools/cmd/DatlinCommand.java
@@ -1,5 +1,6 @@
 package io.datlin.tools.cmd;
 
+import jakarta.annotation.Nonnull;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
@@ -9,6 +10,7 @@ import picocli.CommandLine.Command;
     version = "datlin 1.0"
 )
 public class DatlinCommand extends BaseCommand {
+
     public static void main(String[] args) {
         final CommandLine cmd = new CommandLine(new DatlinCommand());
         cmd.addSubcommand("generate", new GenerateCommand());
